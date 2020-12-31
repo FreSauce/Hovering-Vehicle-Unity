3bcdecb471c35c19a8cf28af2d2a1a61086c8a12
FreSauce <24shardul@gmail.com>
2020-12-31 23:25:47 +0530

    Added camera movement

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Vector3 offset = new Vector3();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
